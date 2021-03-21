e1245827c5aa2aa8cf670d31494a4611ee8c5c81
Corey Smith <58282075+CoreySmithUCR@users.noreply.github.com>
2021-03-21 01:37:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import discord
 import os
-import datetime
 import re
+from datetime import datetime
+from pytz import timezone
+import pytz
+
+
 
 client = discord.Client()
 doodie = '^![Dd]\s{0,}?[Nn&]\s{0,}?[Dd]$'
 dndDays = [int(1), int(5)]
+
+
+def get_pst_time():
+  print("starting get_pst_time")
+  date_format='%m_%d_%Y_%H_%M_%S_%Z'
+  date = datetime.now(tz=pytz.utc)
+  date = date.astimezone(timezone('US/Pacific'))
+  pstDateTime=date.strftime(date_format)
+  print(pstDateTime)
+  print(date.weekday())
+  return date;
+
 
 
 @client.event
@@ -21,14 +37,21 @@ async def on_message(message):
       if str(message.author) == "Corey#5281":
         await message.channel.send('Master why have you called on me again')
       if str(message.author) == "Glacies#1660":
-        await message.channel.send("Thank you Owlek, you always brighten my day with your questions, and for that I am forever in your debt, also")
-      today = datetime.datetime.today().weekday()
+        await message.channel.send("Why have you summoned me today Owlek?")
+      today = get_pst_time().weekday()
       if today in dndDays:
         await message.channel.send('Hold onto your butts, There is DND today')
+      if today == 1:
+        await message.channel.send("We are starting at 8:30 today")
+      if today == 5:
+        await message.channel.send("We are starting at 9:30 today")
+        
       else:
         await message.channel.send("There is no DND today, I'm also slightly disappointed you asked.")
-  
-        
+      
 
+# Programmers: Corey
+# Contributor  Alex
+# Contributor  Alec
 
-client.run(os.getenv('DISCORD_JERETOKEN'))
+client.run(os.getenv('TOKEN'))
